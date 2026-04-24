d17d3160b27f00a360c729bfbc541938c0a7003c
InHo <qheogus55@naver.com>
2026-04-24 21:01:09 +0900

    Initial Settings

---
+++ src/test/java/classenrollmentsystem/ClassEnrollmentSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package classenrollmentsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClassEnrollmentSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
